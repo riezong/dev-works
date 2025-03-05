d6d08b27ddf18f0e303a331b521a14947f745dc1
Andrew <71823699+riezong@users.noreply.github.com>
2025-03-05 20:14:43 +1100

    Change from grid to wrapping flexbox

--- src/index.js
+++ src/index.js
@@ -4,36 +4,13 @@ import { dateDice } from './date-dice';
 import './style.css';
 dateDice();
 
-const nav = document.querySelector('#nav');
-const navItemList = ['Date Dice'];
-for (const item of navItemList) {
-	const navItem = document.createElement('button');
-	navItem.textContent = item;
-	nav.appendChild(navItem);
-}
-
 const tabSwitcher = (function () {
-	// event listeners
-	const btn = document.querySelector('nav');
-	function handleClick(event) {
-		let target = event.target;
-		switch (target.id) {
-			case 'home':
-				console.log('Home menu item was clicked');
-				clearPage();
-				home();
-				break;
-			case 'menu':
-				console.log('Menu menu item was clicked');
-				clearPage();
-				menu();
-				break;
-			case 'about':
-				console.log('About menu item was clicked');
-				clearPage();
-				contact();
-				break;
-		}
+	const nav = document.querySelector('#nav');
+	const navItemList = ['date-dice'];
+	for (const item of navItemList) {
+		const navItem = document.createElement('button');
+		navItem.setAttribute('id', item);
+		navItem.textContent = item.split('-').join(' ');
+		nav.appendChild(navItem);
 	}
-	btn.addEventListener('click', handleClick);
 })();
